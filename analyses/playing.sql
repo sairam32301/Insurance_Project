804a6e4919d5cdfac70d9dd530672d2e3bf7c04d
Saikumar Anumola <saikumaranumola81@gmail.com>
2026-03-19 16:03:12 +0000

    - Updated SLV_ADJUSTER since the Adjuster id has bad data(Scientific data EX: 914E+11) Updated with case statement. - SLV_CUSTOMER Phone having data issues added case statement in transformed CTE -SLV_PAYMENTS removed masked_card_number column which i created the column initially,Updated case statement for card number. -SLV_POLICY lost policy data code recovered from GitHub.

--- analyses/playing.sql
+++ analyses/playing.sql
@@ -1,19 +1,2 @@
- select 
-        card_expiry,
-        card_issuer_bank,
-        card_last4,
-        card_network,
-        card_number,
-        card_type,
-        netbanking_account_mask,
-        netbanking_bank,
-        netbanking_gateway,
-        netbanking_ifsc,
-        netbanking_txn_ref,
-        mode_of_payment,
-        payment_frequency,
-        upi_app,
-        upi_txn_ref,
-        upi_vpa
-
-    from {{ source('raw','BRZ_INSURANCE') }}
+select distinct(source_system)
+from {{ source('raw','BRZ_INSURANCE') }}
---
+++ models/silver/SLV_POLICY.sql
@@ -0,0 +1,69 @@
+{{ config(
+    materialized = 'table'
+) }}
+
+with source_data as (
+
+    select 
+        source_system,
+        policy_id,
+        policy_end_date,
+        policy_number,
+        policy_start_date,
+        policy_status,
+        policy_term_years,
+        policy_type,
+        product_name,
+        premium_amount,
+        sum_assured
+    from {{ source('raw','BRZ_INSURANCE') }}
+
+),
+
+cleaned as (
+
+    select 
+        upper(coalesce(source_system, 'UNKNOWN')) as source_system,
+        case when policy_id like '%E%' then cast(try_to_number(policy_id) as varchar)
+        when policy_id is null or upper(policy_id) like '%P%' then 'UNKNOWN'
+        else policy_id end as policy_id,
+        coalesce(policy_number, 'NA') as policy_number,
+        coalesce(
+            try_to_date(policy_start_date, 'YYYY-MM-DD'),
+            try_to_date(policy_start_date, 'DD-MM-YYYY'),
+            to_date('1900-01-01')
+        ) as policy_start_date,
+        coalesce(
+            try_to_date(policy_end_date, 'YYYY-MM-DD'),
+            try_to_date(policy_end_date, 'DD-MM-YYYY'),
+            to_date('1900-01-01')
+        ) as policy_end_date,
+        upper(coalesce(policy_status, 'UNKNOWN')) as policy_status,
+        coalesce(try_to_number(policy_term_years), 0) as policy_term_years,
+        coalesce(try_to_decimal(premium_amount, 18, 2), 0) as premium_amount,
+        coalesce(try_to_decimal(sum_assured, 18, 2), 0) as sum_assured,
+        upper(coalesce(policy_type, 'UNKNOWN')) as policy_type,
+        upper(coalesce(product_name, 'UNKNOWN')) as product_name
+
+    from source_data
+
+)
+
+
+select 
+    source_system,
+    policy_id,
+    policy_number,
+    policy_start_date,
+    policy_end_date,
+    policy_status,
+    policy_term_years,
+    policy_type,
+    product_name,
+    premium_amount,
+    sum_assured
+from cleaned
+where policy_term_years > 0
+and premium_amount  > 0
+and sum_assured > 0
+and regexp_like(policy_id, '^[0-9]+$')
