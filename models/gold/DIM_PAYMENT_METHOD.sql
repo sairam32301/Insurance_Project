517ef61fece2f20161986c1ef580ecaa5eaf024e
Saikumar Anumola <saikumar.anumola@gds.ey.com>
2026-04-02 22:58:13 +0530

    added card_number column in to DIM_PAYMENT_METHOD

--- models/gold/DIM_PAYMENT_METHOD.sql
+++ models/gold/DIM_PAYMENT_METHOD.sql
@@ -1,9 +1,9 @@
 select 
-    row_number() over (order by MODE_OF_PAYMENT, CARD_TYPE, CARD_NETWORK) as payment_method_id
-    ,CARD_TYPE
-    ,CARD_NETWORK
-    ,UPI_APP
-    ,NETBANKING_BANK
-    ,MODE_OF_PAYMENT
-from {{ref('SLV_PAYMENTS')}}
-qualify row_number() over (partition by MODE_OF_PAYMENT order by CARD_TYPE, CARD_NETWORK) = 1
+    row_number() over (order by MODE_OF_PAYMENT, CARD_TYPE, CARD_NETWORK) as payment_method_id,
+    CARD_TYPE,
+    card_number,
+    CARD_NETWORK,
+    UPI_APP,
+    NETBANKING_BANK,
+    MODE_OF_PAYMENT
+from {{ ref('SLV_PAYMENTS') }}
